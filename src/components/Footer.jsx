483c3a6cbb0cbae149837a1d351e899516b57139
Rafael M. da Silva <123322146+rafaelmdasilva@users.noreply.github.com>
2024-05-26 12:31:37 -0300

    Add contact info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,13 @@ import styles from './Footer.module.css';
 import githubLogo from '../assets/footer/github-mark-white.png';
 import linkedinLogo from '../assets/footer/In-White-128.png';
 import emailLogo from '../assets/footer/envelope-open-svgrepo-com.svg';
+import whatsappLogo from '../assets/footer/whatsapp-color-svgrepo-com.svg';
 
 const Footer = () => {
 	const linkedinLink = 'https://www.linkedin.com/in/rafaelmdasilva451';
 	const githubLink = 'https://github.com/rafaelmdasilva';
 	const email = 'mailto:rafaelmsilva451@outlook.com';
+	const whatsapp = 'https://wa.me/+5548991773697';
 	const gpl = 'http://www.gnu.org/licenses/gpl-3.0.en.html';
 
 	return (
@@ -20,6 +22,9 @@ const Footer = () => {
 				</a>
 				<a href={email} target='_blank'>
 					<img src={emailLogo} alt='Email logo' />
+				</a>
+				<a href={whatsapp} target='_blank'>
+					<img src={whatsappLogo} alt='WhatsApp logo' />
 				</a>
 			</div>
 			<div className={styles.copyright}>
