58f80b226e596fea1fdba8738a182790b6f22189
Rafael M. da Silva <123322146+rafaelmdasilva@users.noreply.github.com>
2024-05-26 12:30:56 -0300

    Add missing alt

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = (props) => {
 	return (
 		<div className={styles.card}>
 			<a href={props.source} target='_blank'>
-				<img className={styles.preview} src={props.preview} alt='' />
+				<img className={styles.preview} src={props.preview} alt='Preview image' />
 			</a>
 			<h2 className={styles.title}>{props.name}</h2>
 			<p className={styles.description}>{props.description}</p>
